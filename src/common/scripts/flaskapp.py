eb269cae8e5dd188556155549664b5947c7d4f08
hvarma <hariharaindukuri@gmail.com>
2019-03-14 02:22:03 -0400

    Adding custom applications for files

--- src/common/scripts/flaskapp.py
+++ src/common/scripts/flaskapp.py
@@ -1,7 +1,10 @@
 import os
 import sys
-from flask import Flask
+from flask import Flask, make_response, jsonify
 app = Flask(__name__)
+
+libreOfficeTypes = set(['doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx'])
+imageTypes = set(['png', 'jpg', 'jpeg'])    
 
 @app.route("/")
 def hello():
@@ -9,13 +12,37 @@ def hello():
 
 @app.route("/file/<fileName>")
 def open_file(fileName):
+    
+    if '.' in fileName:
+        fileType = fileName.rsplit('.', 1)[1].lower()
+    
     try:
-        command = "google-chrome /logs/$POD_NAME/" + fileName +' &'
-        response = os.system(command)
-        return response
+        if fileType in libreOfficeTypes:
+            # sudo apt-get install libreoffice
+            command = "libreoffice ~/Downloads/" + fileName + ' &'
+            response = os.system(command)
+            return response, 200
+        elif fileType == 'pdf':
+            # sudo apt-get install mupdf mupdf-tools
+            command = "mupdf ~/Downloads/" + fileName +' &'
+            response = os.system(command)
+            return response, 200
+        elif fileType == 'txt':
+            # sudo apt install gedit gedit-plugins gedit-common
+            command = "gedit ~/Downloads/" + fileName +' &'
+            response = os.system(command)
+            return response, 200
+        elif fileType in imageTypes:
+            # sudo apt-get install eog
+            command = "eog ~/Downloads/" + fileName +' &'
+            response = os.system(command)
+            return response, 200
+        else:
+            response = make_response(jsonify(message="File type not supported"))
+            return response, 500
     except:
         e = sys.exc_info()[0]
-        return e
+        return e, 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
