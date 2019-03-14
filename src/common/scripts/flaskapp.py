87ee74c0e84db830d77c76ca87523512a7bd2d4a
Sreeraj Krishnan <sreerajkrishnan.k@gmail.com>
2019-03-14 14:05:16 +0530

    added logic to delete the file and terminate the process

--- src/common/scripts/flaskapp.py
+++ src/common/scripts/flaskapp.py
@@ -1,11 +1,12 @@
 import os
+import subprocess
 import sys
 from flask import Flask, make_response, jsonify
 app = Flask(__name__)
 
 libreOfficeTypes = set(['doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx'])
 imageTypes = set(['png', 'jpg', 'jpeg'])    
-
+process = ""
 @app.route("/")
 def hello():
     return "hello"
@@ -20,29 +21,37 @@ def open_file(fileName):
         if fileType in libreOfficeTypes:
             # sudo apt-get install libreoffice
             command = "libreoffice /logs/$POD_NAME/" + fileName + ' &'
-            response = os.system(command)
-            return response, 200
+            process = subprocess.Popen(command.split())
+            return 200
         elif fileType == 'pdf':
             # sudo apt-get install mupdf mupdf-tools
             command = "mupdf /logs/$POD_NAME/" + fileName +' &'
-            response = os.system(command)
-            return response, 200
+            process = subprocess.Popen(command.split())
+            #response = os.system(command)
+            return 200
         elif fileType == 'txt':
             # sudo apt install gedit gedit-plugins gedit-common
             command = "gedit /logs/$POD_NAME/" + fileName +' &'
-            response = os.system(command)
-            return response, 200
+            process = subprocess.Popen(command.split())
+            return 200
         elif fileType in imageTypes:
             # sudo apt-get install eog
             command = "eog /logs/$POD_NAME/" + fileName +' &'
-            response = os.system(command)
-            return response, 200
+            process = subprocess.Popen(command.split())
+            return 200
         else:
             response = make_response(jsonify(message="File type not supported"))
             return response, 500
     except:
         e = sys.exc_info()[0]
         return e, 400
+
+@app.route("/file/<fileName>", methods=['DELETE'])
+def delete_file(fileName):
+    process.terminate()
+    os.remove('/logs/$POD_NAME/'+fileName)
+    return 200
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
